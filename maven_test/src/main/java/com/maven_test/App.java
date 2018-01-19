fe362a2084a1c49db2ff758312042c9dfb169200
torkos89 <torkos89@gmail.com>
2018-01-19 14:10:46 -0600

    Add files via upload

---
+++ maven_test/src/main/java/com/maven_test/App.java
@@ -0,0 +1,8 @@
+package com.maven_test;
+
+public class App {
+  public String demo() { 
+   // System.out.println("tested");
+    return "tested";
+  }
+}
---
+++ maven_test/src/test/java/com/maven_test/test.java
@@ -0,0 +1,15 @@
+package com.maven_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+  @Test
+  public void demo() {
+    System.out.println("test");
+    assertEquals("yes","yes");
+    //fail("Not yet implemented");
+  }
+}
